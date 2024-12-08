145c7e7e34370a14a1dfc6b4ba84479b766fa29a
Mesut Soylu <onexey@gmail.com>
2024-12-08 22:45:28 +0100

    backup exclusion list added

--- backup-to-s3.sh
+++ backup-to-s3.sh
@@ -8,6 +8,40 @@ TAR_TEMP_PATH="/tmp/docker-config"        # Temporary path to store the tar file
 TAR_FILENAME="backup_$TIMESTAMP.tar.gz"   # Name of the tar file
 TAR_FILE="$TAR_TEMP_PATH/$TAR_FILENAME"   # Path to store the tar file
 
+# Exclusion list
+EXCLUDE_LIST=(
+    "**/bazarr/config/backup"
+    "**/bazarr/config/cache"
+    "**/bazarr/config/db"
+    "**/bazarr/config/log"
+    "**/bazarr/config/restore"
+    "**/caddy/config"
+    "**/caddy/data"
+    "**/caddy/site"
+    "**/homepage/logs"
+    "**/jellyfin"
+    "**/pihole"
+    "**/portainer"
+    "**/qbittorrent"
+    "**/radarr/config/Backups"
+    "**/radarr/config/logs"
+    "**/radarr/config/MediaCover"
+    "**/radarr/config/Sentry"
+    "**/sonarr/config/Backups"
+    "**/sonarr/config/logs"
+    "**/sonarr/config/MediaCover"
+    "**/sonarr/config/Sentry"
+)
+
+# Create exclusion parameters for tar
+EXCLUDE_PARAMS=""
+for EXCLUDE in "${EXCLUDE_LIST[@]}"; do
+    EXCLUDE_PARAMS+=" --exclude='$EXCLUDE'"
+done
+
+# Trim leading and trailing whitespace from EXCLUDE_PARAMS
+EXCLUDE_PARAMS=$(echo $EXCLUDE_PARAMS | xargs)
+
 # Ensure the temp directory exists
 mkdir -p $TAR_TEMP_PATH
 
@@ -15,7 +49,7 @@ mkdir -p $TAR_TEMP_PATH
 find $TAR_TEMP_PATH -type f -mtime +30 -exec rm {} \;
 
 # Create a tarball and compress it with gzip
-tar -czf $TAR_FILE -C $(dirname $FOLDER_TO_TAR) $(basename $FOLDER_TO_TAR)
+tar -czf $TAR_FILE $EXCLUDE_PARAMS -C $(dirname $FOLDER_TO_TAR) $(basename $FOLDER_TO_TAR)
 
 # Upload the gzipped tar file to the S3 bucket under the "configs" key
 aws s3 cp $TAR_FILE s3://$BUCKET_NAME/$TAR_FILENAME
